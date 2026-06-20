32c7aabf976ec40d732faa3266e60964295cf578
azureawsforme <azureawsforme@gmail.com>
2026-06-20 19:45:23 +0530

    Added form page

--- server.py
+++ server.py
@@ -1,18 +1,29 @@
 from flask import Flask, request, render_template
 
-invite_app = Flask('app')
+app = Flask('app')
 
-@invite_app.route("/view")
+@app.route("/")
+def form():
+    return render_template("form.html")
 
+
+@app.route("/view")
 def view_invite():
-    to = "Sarah"
-    event = "Chirag's Birthday Party"
-    date = "Feb 17th"
-    time = " 6 PM "
-    sender = "Raghu"
+    # to = "Sarah"
+    # event = "Chirag's Birthday Party"
+    # date = "Feb 17th"
+    # time = " 6 PM "
+    # sender = "Raghu"
+
+    to = request.args.get("to")
+    event = request.args.get("event")
+    date = request.args.get("date")
+    time = request.args.get("time")
+    sender = request.args.get("sender")
+    style = request.args.get('style')
 
     #http://localhost:8080/view?=kids
-    style = request.args.get('style')
+    # style = request.args.get('style')
 
     template = "invite-" + style + ".html"
     
@@ -20,4 +31,4 @@ def view_invite():
     return render_template(template, to=to, event_name=event,date=date,time=time,sender=sender)
 
 if __name__ == '__main__' :
-    invite_app.run(debug=True, use_reloader=True, host='0.0.0.0',port=8080)
+    app.run(debug=True, use_reloader=True, host='0.0.0.0',port=8080)
